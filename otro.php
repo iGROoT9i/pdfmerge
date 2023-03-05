ef9b53a760a3318e24ee2774562788d463798961
iGROoT9i <jhonluna@upeu.edu.pe>
2023-03-05 18:16:36 -0500

    first commit5

--- otro.php
+++ otro.php
@@ -1,7 +1,19 @@
 <?php
- if (isset($_SESSION['message']) && $_SESSION['message'])
- {
-   printf('<b>%s</b>', $_SESSION['message']);
-   unset($_SESSION['message']);
- }
+session_start(); 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP File Upload</title>
+</head>
+<body>
+  <?php
+    if (isset($_SESSION['message']) && $_SESSION['message'])
+    {
+      printf('<b>%s</b>', $_SESSION['message']);
+      unset($_SESSION['message']);
+    }
+  ?>
+  
+</body>
+</html>
